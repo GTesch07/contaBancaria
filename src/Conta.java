353d4e6e33a28161696d712829df999f96e164cc
Guilherme da Silva Tesch <tesch.guilherme@hotmail.com>
2025-07-07 11:59:33 -0300

    Métodos corrigidos.

--- src/Conta.java
+++ src/Conta.java
@@ -27,24 +27,47 @@ public class Conta {
     public void sacar(double valor){
         if (valor <= saldo){
             saldo -= valor;
-        }else if (valor > saldo){
-            System.out.println("Saldo insuficiente.");
+        }else if (valor <= saldo + chequeEspecial){
+            double restante = valor - saldo;
+            saldo = 0;
+            chequeEspecial -= restante;
+            System.out.println("Você usou R$ " + restante + " do cheque especial.");
+        }else{
+            System.out.println("Saldo insuficiente, incluindo o limite do cheque especial.");
         }
     }
 
     public void pagarBoleto(double valor){
-           if (valor <= saldo){
-            saldo -= valor;
-        }else if (valor > saldo){
-            System.out.println("Saldo insuficiente.");
-        }
+       sacar(valor);
+       System.out.println("Boleto pago no valor de R$ " + valor);
     }
 
     public void verificarUsoChequeEspecial(){
-
+        if (chequeEspecial < 50 && saldo <= 500 || chequeEspecial < saldo * 0.5 && saldo > 500){
+            System.out.println("Você está utilizando o cheque especial.");
+        }else{
+            System.out.println("Você não está utilizando o cheque especial.");
+        }
     }
 
     public void cobrarTaxaChequeEspecial(){
+        double limiteOriginal;
+        if(saldo <= 500){
+            limiteOriginal = 50;
+        }else{
+            limiteOriginal = saldo * 0.5;
+        }
 
+        double valorUsado = limiteOriginal - chequeEspecial;
+
+        if (valorUsado > 0) {
+            double taxa = valorUsado * 0.2;
+            if (saldo >= taxa) {
+                saldo -= taxa;
+                System.out.println("Foi cobrada uma taxa de R$ " + taxa + " referente ao uso do cheque especial.");
+            }else{
+                System.out.println("Saldo insuficiente para cobrar a taxa de cheque especial.");
+            }
+        }
     }
 }
